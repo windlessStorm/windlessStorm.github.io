83103bf4638baa7b0f47087a63d082c7ae97e5d2
Windlessstorm <vikrant.biswas@gmail.com>
2018-10-21 18:33:18 +0530

    todo

---
+++ todo-app/js/about.3cb36f54.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},o=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h1",[t._v("TODO task application with VueJS frontend consuming API from Django backend")])])}],u=e("2877"),i={},s=Object(u["a"])(i,a,o,!1,null,null,null);s.options.__file="About.vue";n["default"]=s.exports}}]);
+//# sourceMappingURL=about.3cb36f54.js.map
---
+++ todo-app/precache-manifest.e9f6118cf6f7a5e4032d9729176c000c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a964f316cf5b15924b84",
+    "url": "/js/chunk-vendors.e957f96f.js"
+  },
+  {
+    "revision": "e73abf60b2849de9a4f2",
+    "url": "/js/app.5b2170d6.js"
+  },
+  {
+    "revision": "826f73e5a4ab9bd02a39",
+    "url": "/js/about.3cb36f54.js"
+  },
+  {
+    "revision": "88ba59887056ca966c81a7f47b79631d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e055714d2c647827907bbf144cf409f",
+    "url": "/img/logo.5e055714.png"
+  },
+  {
+    "revision": "a964f316cf5b15924b84",
+    "url": "/css/chunk-vendors.b75fde49.css"
+  },
+  {
+    "revision": "e73abf60b2849de9a4f2",
+    "url": "/css/app.d2c97c26.css"
+  }
+];
